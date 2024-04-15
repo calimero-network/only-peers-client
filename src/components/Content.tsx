93ac3111c3cf8a49252b8ff7d1b7c0fc97573316
Fran Domovic <fran@calimero.network>
2024-04-15 18:32:16 +0200

    fix: merge error

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -28,8 +28,6 @@ import { ResponseData } from "src/api/response";
 import { useRouter } from "next/router";
 import { setStorageNodeAuthorized } from "src/lib/storage";
 import { Loading } from "./Loading";
-import LoginButton from "./button/LoginButton";
-import NearIcon from "./icons/Near";
 
 export interface Message {
   premium: boolean;
@@ -167,149 +165,21 @@ const Content: React.FC = () => {
     const publicKey = urlParams.get("publicKey") as string;
     const signature = urlParams.get("signature") as string;
 
-        const isMessageVerified = verifiedFullKeyBelongsToUser && verifiedSignature;
+    if (!accId && !publicKey && !signature) {
+      console.error("Missing params in url.");
+      return;
+    }
 
-        const resultMessage = isMessageVerified
-            ? "Successfully verified"
-            : "Failed to verify";
+    const message: SignMessageParams = JSON.parse(
+      localStorage.getItem("message")!
+    );
 
-        console.log(
-            `${ resultMessage } signed message: '${ message.message
-            }': \n ${ JSON.stringify(signedMessage) }`
-        );
+    const state: SignatureMessageMetadata = JSON.parse(message.state);
 
-        return isMessageVerified;
-    }, [selector.options.network]);
-
-    const verifyMessageBrowserWallet = useCallback(async () => {
-        const urlParams = new URLSearchParams(
-            window.location.hash.substring(1) // skip the first char (#)
-        );
-        const accId = urlParams.get("accountId") as string;
-        const publicKey = urlParams.get("publicKey") as string;
-        const signature = urlParams.get("signature") as string;
-
-        if (!accId && !publicKey && !signature) {
-            console.error("Missing params in url.");
-            return;
-        }
-
-        const message: SignMessageParams = JSON.parse(
-            localStorage.getItem("message")!
-        );
-
-        const state: SignatureMessageMetadata = JSON.parse(message.state);
-
-        const signedMessage = {
-            accountId: accId,
-            publicKey,
-            signature,
-        };
-
-        const isMessageVerified: boolean = await verifyMessage(message, signedMessage);
-
-        const url = new URL(location.href);
-        url.hash = "";
-        url.search = "";
-        window.history.replaceState({}, document.title, url);
-        localStorage.removeItem("message");
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-
-        if (isMessageVerified) {
-            const signatureMetadata: NearSignatureMessageMetadata = {
-                recipient: message.recipient,
-                callbackUrl: message.callbackUrl,
-                nonce: message.nonce.toString("base64"),
-            };
-            const payload: Payload = {
-                message: state,
-                metadata: signatureMetadata
-            };
-            const walletSignatureData: WalletSignatureData = {
-                payload: payload,
-                clientPubKey: publicKey
-            };
-            const walletMetadata: WalletMetadata = {
-                type: WalletType.NEAR,
-                signingKey: publicKey
-            };
-            const loginRequest: LoginRequest = {
-                walletSignature: signature,
-                payload: walletSignatureData.payload,
-                walletMetadata: walletMetadata
-            };
-
-            await apiClient.node().login(loginRequest).then((result) => {
-                console.log("result", result);
-                if (result.error) {
-                    console.error("login error", result.error);
-                    //TODO handle error
-                } else {
-                    setStorageNodeAuthorized();
-                    router.push("/feed");
-                }
-            }).catch(() => {
-                console.error("error while login");
-                //TODO handle error
-            });
-        } else {
-            //TODO handle error
-            console.error("Message not verified");
-        }
-    }, [router, verifyMessage]);
-
-    async function handleSignMessage() {
-        const challengeResponseData: ResponseData<NodeChallenge> = await apiClient.node().requestChallenge();
-        const {publicKey} = await getOrCreateKeypair();
-
-        if (challengeResponseData.error) {
-            console.log("requestChallenge api error", challengeResponseData.error);
-            return;
-        }
-
-        // Comment out for now as we are not showing wallet selector
-        //const wallet = await selector.wallet();
-        // Predefine wallet selector
-        const wallet = await selector.wallet("my-near-wallet");
-        const nonce = Buffer.from(crypto.getRandomValues(new Uint8Array(32)));
-        const recipient = appName;
-        const callbackUrl = location.href;
-        const applicationId = challengeResponseData.data.applicationId;
-        const nodeSignature = challengeResponseData.data.nodeSignature;
-        const timestamp = challengeResponseData.data.timestamp;
-
-
-        const signatureMessage: SignatureMessage = {
-            nodeSignature,
-            clientPublicKey: publicKey
-        };
-        const message: string = JSON.stringify(signatureMessage);
-
-        const state: SignatureMessageMetadata = {
-            clientPublicKey: publicKey,
-            nodeSignature,
-            nonce: nonce.toString("base64"),
-            applicationId,
-            timestamp,
-            message
-        };
-
-        if (wallet.type === "browser") {
-            console.log("browser");
-
-            localStorage.setItem(
-                "message",
-                JSON.stringify({
-                    message,
-                    nonce: [...nonce],
-                    recipient,
-                    callbackUrl,
-                    state: JSON.stringify(state),
-                })
-            );
-        }
-
-        await wallet.signMessage({message, nonce, recipient, callbackUrl});
+    const signedMessage = {
+      accountId: accId,
+      publicKey,
+      signature,
     };
 
     const isMessageVerified: boolean = await verifyMessage(
@@ -383,19 +253,45 @@ const Content: React.FC = () => {
     }
 
     // Comment out for now as we are not showing wallet selector
-    // if (!account) {
-    //     return (
-    //         <Fragment>
-    //             <div>
-    //                 <Button
-    //                     title="Log in with NEAR"
-    //                     onClick={handleSignIn}
-    //                     backgroundColor={""}
-    //                     backgroundColorHover={""} />
-    //             </div>
-    //         </Fragment>
-    //     );
-    // }
+    //const wallet = await selector.wallet();
+    // Predefine wallet selector
+    const wallet = await selector.wallet("my-near-wallet");
+    const nonce = Buffer.from(crypto.getRandomValues(new Uint8Array(32)));
+    const recipient = appName;
+    const callbackUrl = location.href;
+    const applicationId = challengeResponseData.data.applicationId;
+    const nodeSignature = challengeResponseData.data.nodeSignature;
+    const timestamp = challengeResponseData.data.timestamp;
+
+    const signatureMessage: SignatureMessage = {
+      nodeSignature,
+      clientPublicKey: publicKey,
+    };
+    const message: string = JSON.stringify(signatureMessage);
+
+    const state: SignatureMessageMetadata = {
+      clientPublicKey: publicKey,
+      nodeSignature,
+      nonce: nonce.toString("base64"),
+      applicationId,
+      timestamp,
+      message,
+    };
+
+    if (wallet.type === "browser") {
+      console.log("browser");
+
+      localStorage.setItem(
+        "message",
+        JSON.stringify({
+          message,
+          nonce: [...nonce],
+          recipient,
+          callbackUrl,
+          state: JSON.stringify(state),
+        })
+      );
+    }
 
     await wallet.signMessage({ message, nonce, recipient, callbackUrl });
   }
@@ -404,21 +300,20 @@ const Content: React.FC = () => {
     return <Loading />;
   }
 
-  if (!account) {
-    return (
-      <Fragment>
-        <div>
-          <LoginButton
-            title="Near Wallet"
-            onClick={handleSignIn}
-            backgroundColor={"bg-[#D1D5DB]"}
-            textColor={"text-black px-10"}
-            icon={<NearIcon />}
-          />
-        </div>
-      </Fragment>
-    );
-  }
+  // Comment out for now as we are not showing wallet selector
+  // if (!account) {
+  //     return (
+  //         <Fragment>
+  //             <div>
+  //                 <Button
+  //                     title="Log in with NEAR"
+  //                     onClick={handleSignIn}
+  //                     backgroundColor={""}
+  //                     backgroundColorHover={""} />
+  //             </div>
+  //         </Fragment>
+  //     );
+  // }
 
   return (
     <Fragment>

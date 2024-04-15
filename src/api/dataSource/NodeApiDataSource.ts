c43a677555175f07518047644cc815f263559aa7
matej.vukosav <matej.vukosav@gmail.com>
2024-04-15 22:13:18 +0200

    feat: use challenge endpoing

--- src/api/dataSource/NodeApiDataSource.ts
+++ src/api/dataSource/NodeApiDataSource.ts
@@ -1,38 +1,36 @@
-import {HttpClient} from "../httpClient";
-import {NodeChallenge, LoginResponse, NodeApi, WalletSignatureData, LoginRequest} from "../nodeApi";
-import {ApiResponse, ResponseData} from "../response";
+import { HttpClient } from "../httpClient";
+import {
+  NodeChallenge,
+  LoginResponse,
+  NodeApi,
+  LoginRequest,
+} from "../nodeApi";
+import { ApiResponse } from "../response";
 
 export class NodeApiDataSource implements NodeApi {
-    private client: HttpClient;
+  private client: HttpClient;
 
-    constructor (client: HttpClient) {
-        this.client = client;
-    }
+  constructor(client: HttpClient) {
+    this.client = client;
+  }
 
-    async requestChallenge(): ApiResponse<NodeChallenge> {
-        const apiResponse: ResponseData<NodeChallenge> = {
-            data: {
-                nonce: Buffer.from("challenge123"),
-                applicationId: "app123",
-                timestamp: Math.floor(Date.now() / 1000),
-                nodeSignature: "abcdefhgjsdajbadk"
-            }
-        };
-        return apiResponse;
+  async requestChallenge(): ApiResponse<NodeChallenge> {
+    return await this.client.post<NodeChallenge>(
+      `http://localhost:2428/admin-api/request-challenge`,
+      {
+        applicationId: process.env["NEXT_APPLICATION_ID"],
+      }
+    );
+  }
 
-        // return await this.client.get<Challenge>(
-        //     `http://matej/api/request_challenge`,
-        // );
-    }
+  async login(loginRequest: LoginRequest): ApiResponse<LoginResponse> {
+    console.log("Send request to node with params", loginRequest);
 
-    async login(loginRequest: LoginRequest): ApiResponse<LoginResponse> {
-        console.log("Send request to node with params", loginRequest);
-
-        return await this.client.post<LoginRequest>(
-            `http://localhost:2428/admin-api/add-client-key`,
-            {
-                ...loginRequest
-            }
-        );
-    }
+    return await this.client.post<LoginRequest>(
+      `http://localhost:2428/admin-api/add-client-key`,
+      {
+        ...loginRequest,
+      }
+    );
+  }
 }

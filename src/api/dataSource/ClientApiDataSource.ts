9cd6e22158223ce3be6d1f8e5a8c08359d7d7f66
matej.vukosav <matej.vukosav@gmail.com>
2024-04-17 03:54:51 +0200

    feat: add auth headers

--- src/api/dataSource/ClientApiDataSource.ts
+++ src/api/dataSource/ClientApiDataSource.ts
@@ -1,63 +1,121 @@
-import { ClientApi, ClientMethod, CreateCommentRequest, CreatePostRequest, FeedRequest, PostRequest } from "../clientApi";
+import {
+  ClientApi,
+  ClientMethod,
+  CreateCommentRequest,
+  CreatePostRequest,
+  FeedRequest,
+  PostRequest,
+} from "../clientApi";
 import { ApiResponse } from "../response";
 import { Post } from "src/types/types";
-import { JsonRpcClient } from "@calimero-is-near/calimero-p2p-sdk/lib";
+import {
+  JsonRpcClient,
+  RequestConfig,
+} from "@calimero-is-near/calimero-p2p-sdk/lib";
+import { AxiosHeaders } from "axios";
+import { createAuthHeader } from "src/crypto/crypto";
 
 export function getJsonRpcClient() {
-    return new JsonRpcClient(
-        process.env["NEXT_PUBLIC_RPC_BASE_URL"],
-        process.env["NEXT_PUBLIC_RPC_PATH"],
-    );
+  return new JsonRpcClient(
+    process.env["NEXT_PUBLIC_RPC_BASE_URL"],
+    process.env["NEXT_PUBLIC_RPC_PATH"]
+  );
 }
 
-const applicationId = process.env["NEXT_APPLICATION_ID"]
+const applicationId = process.env["NEXT_PUBLIC_APPLICATION_ID"];
 
 export class ClientApiDataSource implements ClientApi {
-    async fetchFeed(params: FeedRequest): ApiResponse<Post[]> {
-        const response = await getJsonRpcClient().query<FeedRequest, Post[]>({
-            applicationId: applicationId,
-            method: ClientMethod.POSTS,
-            argsJson: params
-        });
-        return {
-            data: response.output,
-            error: null
-        };
-    }
+  async fetchFeed(params: FeedRequest): ApiResponse<Post[]> {
+    const authHeaders = await createAuthHeader(JSON.stringify(params));
 
-    async fetchPost(params: PostRequest): ApiResponse<Post> {
-        const response = await getJsonRpcClient().query<PostRequest, Post>({
-            applicationId: applicationId,
-            method: ClientMethod.POST,
-            argsJson: params
-        });
-        return {
-            data: response.output,
-            error: null
-        };
-    }
+    const headers = new AxiosHeaders();
+    headers.set(
+      "wallet_type",
+      "wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww",
+      true
+    );
+    headers.set("signing_key", "s");
+    headers.set("signature", "ss");
+    headers.set("challenge", "xx");
+    headers.set("Authorization", "dinamo");
 
-    async createPost(params: CreatePostRequest): ApiResponse<Post> {
-        const response = await getJsonRpcClient().mutate<CreatePostRequest, Post>({
-            applicationId: applicationId,
-            method: ClientMethod.CREATE_POST,
-            argsJson: params
-        });
-        return {
-            data: response.output,
-            error: null
-        };
-    }
+    console.log("headers", headers);
 
-    async createComment(params: CreateCommentRequest): ApiResponse<Comment> {
-        const response = await getJsonRpcClient().mutate<CreateCommentRequest, Comment>({
-            applicationId: applicationId,
-            method: ClientMethod.CREATE_COMMENT,
-            argsJson: params
-        });
-        return {
-            data: response.output,
-            error: null
-        };
-    }
+    const config: RequestConfig = {
+      headers: headers,
+    };
+
+    let params22: CreatePostRequest = {
+      title: "ddd",
+      content: "sss",
+    };
+
+    const response = await getJsonRpcClient().mutate<CreatePostRequest, Post>(
+      {
+        applicationId: applicationId,
+        method: ClientMethod.CREATE_POST,
+        argsJson: params22,
+      },
+      config
+    );
+    return {
+      data: [],
+      error: null,
+    };
+
+    // const response = await getJsonRpcClient().query<FeedRequest, Post[]>(
+    //   {
+    //     applicationId: applicationId,
+    //     method: ClientMethod.POSTS,
+    //     argsJson: params,
+    //   },
+    //   config
+    // );
+
+    // console.log("response", response);
+
+    // return {
+    //   data: response.result.output,
+    //   error: null,
+    // };
+  }
+
+  async fetchPost(params: PostRequest): ApiResponse<Post> {
+    const response = await getJsonRpcClient().query<PostRequest, Post>({
+      applicationId: applicationId,
+      method: ClientMethod.POST,
+      argsJson: params,
+    });
+    return {
+      data: response.result.output,
+      error: null,
+    };
+  }
+
+  async createPost(params: CreatePostRequest): ApiResponse<Post> {
+    const response = await getJsonRpcClient().mutate<CreatePostRequest, Post>({
+      applicationId: applicationId,
+      method: ClientMethod.CREATE_POST,
+      argsJson: params,
+    });
+    return {
+      data: response.result.output,
+      error: null,
+    };
+  }
+
+  async createComment(params: CreateCommentRequest): ApiResponse<Comment> {
+    const response = await getJsonRpcClient().mutate<
+      CreateCommentRequest,
+      Comment
+    >({
+      applicationId: applicationId,
+      method: ClientMethod.CREATE_COMMENT,
+      argsJson: params,
+    });
+    return {
+      data: response.result.output,
+      error: null,
+    };
+  }
 }
--- src/crypto/crypto.ts
+++ src/crypto/crypto.ts
@@ -1,46 +1,81 @@
-import {ClientKey, getStorageClientKey as getStoragePrivateKey} from '../lib/storage';
-import {unmarshalPrivateKey} from "@libp2p/crypto/keys";
-import {PrivateKey} from '@libp2p/interface';
-import bs58 from 'bs58';
+import {
+  ClientKey,
+  getStorageClientKey as getStoragePrivateKey,
+} from "../lib/storage";
+import { unmarshalPrivateKey } from "@libp2p/crypto/keys";
+import { PrivateKey } from "@libp2p/interface";
+import bs58 from "bs58";
 
-export interface SignedMessageObject {
-    signature: string;
-    challenge: string;
+export enum WalletType {
+  NEAR = "NEAR",
+  ETH = "ETH",
 }
 
-export async function signMessage(content: string): Promise<SignedMessageObject | null> {
-    const privateKey = await getPrivateKey();
+export interface Header {
+  [key: string]: string;
+}
 
-    if (!privateKey) {
-        return null;
-    }
+export interface AuthHeader {
+  walletTypeHeader: Header;
+  signingKeyHeader: Header;
+  signatureHeader: Header;
+  challengeHeader: Header;
+}
 
-    const encoder = new TextEncoder();
-    const contentBuff = encoder.encode(content);
+export async function createAuthHeader(
+  payload: string
+): Promise<AuthHeader | null> {
+  const privateKey: PrivateKey = await getPrivateKey();
 
-    const hashBuffer = await crypto.subtle.digest('SHA-256', contentBuff);
-    const hashArray = new Uint8Array(hashBuffer);
+  if (!privateKey) {
+    return null;
+  }
 
-    const signature = await privateKey.sign(hashArray);
+  const encoder = new TextEncoder();
+  const contentBuff = encoder.encode(payload);
 
-    const signatureBase58 = bs58.encode(signature);
-    const contentBase58 = bs58.encode(hashArray);
+  const signing_key = bs58.encode(privateKey.public.bytes);
 
-    return {
-        signature: signatureBase58,
-        challenge: contentBase58
-    };
+  const hashBuffer = await crypto.subtle.digest("SHA-256", contentBuff);
+  const hashArray = new Uint8Array(hashBuffer);
+
+  const signature = await privateKey.sign(hashArray);
+  const signatureBase58 = bs58.encode(signature);
+  const contentBase58 = bs58.encode(hashArray);
+
+  let walletTypeHeader: Header = {
+    wallet_type: WalletType.NEAR,
+  };
+
+  let signingKeyHeader: Header = {
+    signing_key: signing_key,
+  };
+
+  let challengeHeader: Header = {
+    challenge: contentBase58,
+  };
+
+  let signatureHeader: Header = {
+    signature: signatureBase58,
+  };
+
+  return {
+    walletTypeHeader,
+    signingKeyHeader,
+    signatureHeader,
+    challengeHeader,
+  };
 }
 
 export async function getPrivateKey(): Promise<PrivateKey | null> {
-    try {
-        const clientKey: ClientKey | null = getStoragePrivateKey();
-        if (!clientKey) {
-            return null;
-        }
-        return await unmarshalPrivateKey(bs58.decode(clientKey.privateKey));
-    } catch (error) {
-        console.error('Error extracting private key:', error);
-        return null;
+  try {
+    const clientKey: ClientKey | null = getStoragePrivateKey();
+    if (!clientKey) {
+      return null;
     }
+    return await unmarshalPrivateKey(bs58.decode(clientKey.privateKey));
+  } catch (error) {
+    console.error("Error extracting private key:", error);
+    return null;
+  }
 }

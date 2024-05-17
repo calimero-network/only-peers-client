a3f3aefaa6085248a84dd182bfe03860ba2e217d
matej.vukosav <matej.vukosav@gmail.com>
2024-05-17 14:52:40 +0200

    chore: clean wokflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  distDir: "build",
   output: "export",
   images: {
     unoptimized: true, // This is needed because GitHub Pages does not support Next.js optimized images
